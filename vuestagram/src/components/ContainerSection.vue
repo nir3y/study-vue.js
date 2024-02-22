4a16a93c440ee265bd07ed7e4af5479ed3d076f6
nir3y <yerinlinlyn@gmail.com>
2024-02-22 15:54:18 +0900

    240222: vuestagram upload

---
+++ vuestagram/src/components/ContainerSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <PostSection />
+    <PostSection /> 
+    <PostSection />
+  </div>
+</template>
+
+<script>
+import PostSection from "./PostSection.vue";
+
+export default {
+  components: {
+    PostSection,
+  },
+};
+</script>
+
+<style>
+</style>
